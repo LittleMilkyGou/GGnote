4240346d52fdf8344122219ad722fc6baf3e3139
Gary <flysilence@outlook.com>
2025-01-20 00:39:13 +1100

    enable next.js static site generation

--- gagatodo/next.config.ts
+++ gagatodo/next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const isProd = process.env.NODE_ENV === 'production';
+
+const internalHost = process.env.TAURI_DEV_HOST || 'localhost';
+
+/** @type {import('next').NextConfig} */
+const nextConfig:NextConfig = {
+  // Ensure uses SSG
+  output: 'export',
+  // Ensure the Next.js Image component in SSG mode
+  images: {
+    unoptimized: true,
+  },
+  // Configure assetPrefix
+  assetPrefix: isProd ? undefined : `http://${internalHost}:3000`,
 };
 
 export default nextConfig;
